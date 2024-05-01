e82a361a548bc83cf731bef3ae1712c529ed9316
MA. EARL FRESKKIE ENCARNACION <2234455@slu.edu.ph>
2024-05-01 11:14:33 +0800

    Removed the white panel

--- src/main/java/View/Design/GradientSliderUI.java
+++ src/main/java/View/Design/GradientSliderUI.java
@@ -8,6 +8,7 @@ import java.awt.geom.Point2D;
 public class GradientSliderUI extends BasicSliderUI {
     public GradientSliderUI(JSlider b) {
         super(b);
+        b.setBackground(new Color(255, 204, 213));
     }
 
     @Override
@@ -15,7 +16,6 @@ public class GradientSliderUI extends BasicSliderUI {
         Graphics2D g2d = (Graphics2D) g;
         Rectangle trackBounds = trackRect;
 
-        // Calculate the color based on the value
         int value = slider.getValue();
         int maximum = slider.getMaximum();
         int minimum = slider.getMinimum();
@@ -26,20 +26,34 @@ public class GradientSliderUI extends BasicSliderUI {
         Color colorTo = Color.GREEN;
         Color trackColor = blendColor(colorFrom, colorTo, fraction);
 
-        // Create the gradient paint
         GradientPaint gradient = new GradientPaint(
                 new Point2D.Float(trackBounds.x, trackBounds.y),
                 colorFrom,
                 new Point2D.Float(trackBounds.x + trackBounds.width, trackBounds.y),
                 colorTo);
 
-        // Fill the track
         g2d.setPaint(gradient);
         g2d.fillRect(trackBounds.x, trackBounds.y + trackBounds.height / 2 - 4,
                 trackBounds.width, 8);
 
-        // Call super to handle thumb painting and other details
         super.paintTrack(g);
+    }
+
+    @Override
+    public void paintThumb(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.WHITE);
+
+        // Optionally, adjust the thumb dimensions and positioning as needed
+        int thumbHeight = 20;
+        int thumbWidth = 10;
+        int thumbX = thumbRect.x + (thumbRect.width - thumbWidth) / 2;
+        int thumbY = thumbRect.y + (thumbRect.height - thumbHeight) / 2;
+
+        g2d.fillRect(thumbX, thumbY, thumbWidth, thumbHeight);
+
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(thumbX, thumbY, thumbWidth, thumbHeight);
     }
 
     private Color blendColor(Color color1, Color color2, float ratio) {
